29cacbd8fc6708907e0d6f6687b7a72a25726b17
Gustavo <gguu.henri@gmail.com>
2025-03-15 11:01:07 -0300

    nao mudei nada

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function buscarEMostrarVideos() {
   try {
     const busca = await axios.get(urlVideos);
 
-    const videos = busca.data;
+    const videos = busca.data; /*tanto faz */
 
     videos.forEach((video) => {
       if (video.categoria == "") {
